e5848cf11b65ef0d4c646e81ab32764c5c42b95b
ryosuke-terasaka1 <terasaka_ryosuke@outlook.jp>
2022-07-24 01:15:24 +0900

    abc261 answer d

--- ABC_20220723/abc261/d/d.py
+++ ABC_20220723/abc261/d/d.py
@@ -1,23 +1,16 @@
 n, m = map(int, input().split())
-
 x = list(map(int, input().split()))
 
-dp = [0] * (n)
-dp[0] = x[0]
-bonus_dic = {}
-bonus_sum = 0
+dp = [[0]*(n+1) for _ in range(n+1)]
+bonus = [0] * (n+1)
 
 for _ in range(m):
     c, y = map(int, input().split())
-    bonus_dic[c] = y
-    bonus_sum += y
+    bonus[c] = y
 
-for i in range(1, n):
-    for k, v in bonus_dic.items():
-        if i == k:
-            dp[i] = max(dp[i-1]+x[i], x[i]+v, dp[i])
-        elif i > k > 1:
-            dp[i] = max(dp[i-k-1]+sum(x[i-k+1:i+1])+v, dp[i], dp[i-1]+x[i]+bonus_sum)
-        else:
-            dp[i] = max(dp[i-1]+x[i], dp[i])
-print(max(dp[n-1], (bonus_sum+sum(x))))
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i][j]=dp[i-1][j-1]+x[i-1]+bonus[j]
+    dp[i][0] = max(dp[i-1])
+
+print(max(dp[n]))
